afed54e7c1934398c02a9fbfff062476876d33b4
AndreiYnov <andrei.radocea@ynov.com>
2017-11-21 15:23:45 +0100

    Exercice 1 correction

--- src/shop-tools.js
+++ src/shop-tools.js
@@ -1,9 +1,10 @@
-function(getPrices) {(
-
-	expect({banana: 1}).to.eql({banana: 1}),
-	expect({potato: 2}).to.eql({potato: 2}),
-	expect({tomato: 3}).to.eql({tomato: 3}),
-	expect({cucumber: 4}).to.eql({cucumber: 4}),
-	expect({salad: 5}).to.eql({salad: 5}),
-	expect({apple: 6}).to.eql({apple: 6}); 
-)}
+exports.getPrices = function() {
+	return {
+		banana: 1,
+		potato : 2,
+		tomato : 3,
+		cucumber : 4,
+		salad : 5,
+		apple : 6
+	};
+};
--- test/shop-tools.test.js
+++ test/shop-tools.test.js
@@ -1,2 +1,18 @@
-// For example
-console.log(getPrices()) // { banana: 1, potato: 2, tomato: 3, cucumber: 4, salad: 5, apple: 6 }
+var expect = require("chai").expect;
+
+var getPrices = require("../src/shop-tools.js").getPrices()
+
+describe("getPrices", function(){
+    var products = getPrices();
+
+    it("Should return an object wich describes the"){
+        expect(products).to.eql({
+		banana: 1,
+		potato : 2,
+		tomato : 3,
+		cucumber : 4,
+		salad : 5,
+		apple : 6
+        })
+    }
+})
